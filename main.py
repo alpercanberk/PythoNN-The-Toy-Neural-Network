dccd40e40dcd5cf59761dac9e3b7ad60d2bd17e8
Alper Canberk <alpercanberk@MacBook-Pro-6.local>
2018-07-23 15:32:24 +0300

    dynamic activation function added to feedforward

--- main.py
+++ main.py
@@ -1,61 +1,70 @@
 import numpy as np
 
-def sigmoid(x):
-    return 1.0/(1+ np.exp(-x))
-
-def sigmoid_derivative(x):
-    return x * (1.0 - x)
-
 class NeuralNetwork:
     def __init__(self, nodes):
-        self.weights = []             
+        self.weights = []
+        self.biases = []           
         self.lr = 0.1
 
         for layer_index in range(0, len(nodes)-1):
-        	node_count = nodes[layer_index]
-        	next_node_count = nodes[layer_index+1]
-        	self.weights.append(np.matrix(np.random.rand(next_node_count, node_count)))
-
+            node_count = nodes[layer_index]
+            next_node_count = nodes[layer_index+1]
+            self.weights.append(np.matrix(np.random.rand(next_node_count, node_count)))
+            self.biases.append(np.random.randn())
         print "\narray of weights:"
         for i in range(0, len(self.weights)):
-        	print("weights "+str(i)+":")
-        	print(self.weights[i])
+            print("weights "+str(i)+":")
+            print(self.weights[i])
         print "---------\n" 
 
+    def activation_function(x):
+        return 1.0/(1+ np.exp(-x))
+
+    def activation_function_derivative(x):
+        return x * (1.0 - x)
+        
     def feedforward(self, input_array):
-    	layer_vals = [np.matrix(input_array).T]
-    	print "input:"
-    	print layer_vals[0]
-    	print ""
-    	for weights_i in range(0, len(self.weights)):
+        layer_vals = [np.matrix(input_array).T]
+        print "input:"
+        print layer_vals[0]
+        print ""
+        for weights_i in range(0, len(self.weights)):
 
-    		weights = self.weights[weights_i]
-    		values = layer_vals[weights_i]
-    		
+            weights = self.weights[weights_i]
+            bias = self.biases[weights_i]
+            values = layer_vals[weights_i]
+            
+            #Y = activationFunction(W * I + B)
+            y = self.activation_function(np.add(np.dot(weights, values), bias))
 
-    		print str(weights_i) + ":"
-    		print weights
-    		print values
-    		
-    		print sigmoid(np.dot(weights, values))
+            print str(weights_i) + ":"
+            print "weights:"
+            print weights
+            print "bias:"
+            print bias
+            print "values:"
+            print values
+            print "result:"
 
-    		layer_vals.append(sigmoid(np.dot(weights, values)))
+            print y
 
-    	print "feedforward:\n"
+            layer_vals.append(y)
+
+        print "feedforward:\n"
         for i in range(0, len(layer_vals)):
-        	print "layer vals "+str(i)+":"
-        	print layer_vals[i]
+            print "layer vals "+str(i)+":"
+            print layer_vals[i]
 
-    	return layer_vals[-1]
+        return layer_vals[-1]
 
     def backprop(self, inputs, targets):
-    	print("BACKPROP STARTING")
-    	outputs = self.feedforward(inputs)
-    	targets = np.matrix(targets)
+        print("BACKPROP STARTING")
+        outputs = self.feedforward(inputs)
+        targets = np.matrix(targets)
 
-    	output_errors = np.subtract(targets, outputs)
+        output_errors = np.subtract(targets, outputs)
 
-    	print "BACKPROP!"
+        print "BACKPROP!"
         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
         
         # update the weights with the derivative (slope) of the loss function
@@ -71,13 +80,12 @@ if __name__ == "__main__":
                   [1,1,0]])
     y = np.array([[0],[1],[1],[0]])
     nn = NeuralNetwork([3,2,1])
-
     
     print(nn.feedforward([0,1,0]))
     nn.backprop(
-    	[[1, 0, 1],
-    	[0, 0, 1]],
-    	[[1,0]]
-    	)
+        [[1, 0, 1],
+        [0, 0, 1]],
+        [[1,0]]
+        )
 
 
